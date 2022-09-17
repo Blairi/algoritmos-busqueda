c522414157741879b09b3804548757be0d4f99be
Blairi <axel.avi.fer@gmail.com>
2022-09-16 22:29:22 -0500

    agregando contadores

--- práctica4/busquedaBinaria.py
+++ práctica4/busquedaBinaria.py
@@ -15,6 +15,31 @@ def busquedaBinariaIterativa(A, key, attr):
             i = m + 1
     return False
 
+
+def timesBusquedaBinariaIterativa(A, key, attr):
+
+    times = 0
+
+    i = 0
+    f = len( A ) - 1
+
+    while f >= i:
+
+        times += 1
+
+        m = ( f + i ) // 2
+        if attr(A[m]) == key:
+            return m, times
+
+        if attr(A[m]) > key:
+            f = m - 1
+        else:
+            i = m + 1
+
+    return False, times
+
+
+
 def busquedaBinariaRecursiva(A, key, attr, inicio, final):
     if inicio > final:
         return False
@@ -29,7 +54,48 @@ def busquedaBinariaRecursiva(A, key, attr, inicio, final):
 
     else:
         return busquedaBinariaRecursiva(A, key, attr, m + 1, final)
+        
 
+def timesBusquedaBinariaRecursiva(A, key, attr, inicio, final):
+
+    times = 0
+    times += 1
+
+    if inicio > final:
+        return False, times
+
+    m = (inicio + final) // 2
+    
+    if attr(A[m]) == key:
+        return m, times
+
+    if attr(A[m]) > key:
+        ret, time = timesBusquedaBinariaRecursiva(A, key, attr, inicio, m - 1)
+        times += time
+        return ret, times
+
+    else:
+        ret, time = timesBusquedaBinariaRecursiva(A, key, attr, m + 1, final)
+        times += time
+        return ret, times
+    
+
+# Prueba con contador
+# A = []
+# for i in range(10):
+#     A.append(Jugador(i * 3, "Pedro"))
+
+# for jugador in A:
+#     print(jugador)
+
+# x, times = timesBusquedaBinariaIterativa(A, 12, Jugador.getScore)
+# print(f"Se tardo {times} en encontrar {x}")
+
+# x, times = timesBusquedaBinariaRecursiva(A, 9, Jugador.getScore, 0, len(A) - 1)
+# print(f"Se tardo {times} en encontrar {x}")
+
+
+# Prueba
 # A = []
 # for i in range(10):
     # A.append(Jugador(i * 3, "Pedro"))
--- práctica4/busquedaLineal.py
+++ práctica4/busquedaLineal.py
@@ -6,9 +6,18 @@ def busquedaLinealIterativa(A, attr, key):
             return i
     return False
 
+def timesBusquedaLinealIterativa(A, attr, key):
+    times = 0
+    for i in range(len(A)):
+        times += 1
+        if key == attr(A[i]):
+            return i, times
+    return False, times
+
+
 def busquedaLinealRecursiva(A, attr, key, indice):
 
-    if indice >= len(A) - 1:
+    if indice >= len(A):
         return False
 
     if key == attr(A[indice]):
@@ -18,14 +27,48 @@ def busquedaLinealRecursiva(A, attr, key, indice):
 
     return busquedaLinealRecursiva(A, attr, key, indice)
 
+def timesBusquedaLinealRecursiva(A, attr, key, indice):
 
+    times = 0
+    times += 1
+
+    if indice >= len(A):
+        return False, times
+
+    if key == attr(A[indice]):
+        return indice, times
+
+    indice = indice + 1
+
+    ret, time = timesBusquedaLinealRecursiva(A, attr, key, indice)
+    times += time
+    return ret, times
+
+
+# Prueba con contador
 # A = []
 # for i in range(10):
-   # A.append(Jugador(i * 3, "Pedro"))
+#    A.append(Jugador(i * 3, "Pedro"))
 
 # for jugador in A:
-    # print(jugador)
+#     print(jugador)
+
+# x, times = timesBusquedaLinealIterativa(A, Jugador.getScore, 27)
+# print(f"Se tardo {times} en encontrar {x}")
+
+# x, times = timesBusquedaLinealRecursiva(A, Jugador.getScore, 27, 0)
+# print(f"Se tardo {times} en encontrar {x}")
+
+
+
+# Prueba
+# A = []
+# for i in range(10):
+#    A.append(Jugador(i * 3, "Pedro"))
+
+# for jugador in A:
+#     print(jugador)
 
 # print(busquedaLinealIterativa(A, Jugador.getScore, 6))
-# print(busquedaLinealRecursiva(A, Jugador.getScore, 6, 0))
+# print(busquedaLinealRecursiva(A, Jugador.getScore, 27, 0))
 
