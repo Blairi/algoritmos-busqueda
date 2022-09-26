de52c456391e3245855184036890bd6351eaf7f0
Blairi <axel.avi.fer@gmail.com>
2022-09-26 13:23:15 -0500

    modificando función

--- práctica5/busquedaHash.py
+++ práctica5/busquedaHash.py
@@ -1,60 +1,77 @@
 
-def formarTabla(m):
-    T = [None] * m
+def formarTabla(longitud):
+    T = [None] * longitud
     return T
 
 
-def convertirLLave(x):
+def convertirLLave(llave):
     keyNum = 0
     i = 0
-
-    for char in x:
+    for char in llave:
         keyNum = keyNum + ord(char) * i
         i = i + 1
 
     return keyNum
 
-def h(x, m):
-    return x % m
+def h(llave, longitud):
+    return llave % longitud
 
-def insertar(T, m, x, valor):
+def insertar(T, longitud, llave, valor):
+
+    h1 = h(convertirLLave(llave), longitud)
+
     j = 0
-    h1 = h(convertirLLave(x), m)
-    while j < m:
-        indice = (h1 + j) % m
-        par =[x, valor]
-        if  T[indice] == None:
-            T[indice] = par
+    while j < longitud:
+
+        indice = (h1 + j) % longitud
+        llaveValor = [llave, valor]
+
+        if T[indice] != None and T[indice][0] == llave: # Si ya existe la llave, reasignamos el nuevo valor
+            T[indice] = llaveValor
             return indice
-        else:
-            j = j + 1
+
+        if  T[indice] == None:
+            T[indice] = llaveValor
+            return indice
+        
+        j = j + 1
+        
     print("No hay lugar")
     return -1
 
-def buscar(T, m, x, valor):
+def buscar(T, longitud, llave):
+
+    h1 = h(convertirLLave(llave), longitud)
+
     j = 0
-    h1 = h(convertirLLave(x), m)
-    while j < m:
-        indice = (h1 + j) % m
-        if T[indice] != None:
-            if T[indice][0] == x:
-                return indice
-            else:
-                j = j + 1
+    while j < longitud:
+    
+        indice = (h1 + j) % longitud
+
+        if T[indice] != None and T[indice][0] == llave:
+            return indice
+        else:
+            j = j + 1
+
         return -1
+
     return -1
 
-def main():
-    m = 5
-    T = formarTabla(m)
-    print(T)
-    insertar(T, m, "Hola1", "122121")
-    insertar(T, m, "Hola2", "122611")
-    insertar(T, m, "Hola3", "122231")
-    insertar(T, m, "Hola4", "122121")
-    insertar(T, m, "Hola5", "122651")
-    insertar(T, m, "Hola6", "122321")
-    print(T)
-    print(buscar(T, m, "Hola5", "122651"))
+# def buscar(T, longitud, llave):
 
-main()
+#     h1 = h(convertirLLave(llave), longitud)
+
+#     j = 0
+#     while j < longitud:
+#         indice = (h1 + j) % longitud
+
+#         if T[indice] != None:
+            
+#             if T[indice][0] == llave:
+#                 return indice
+#             else:
+#                 j = j + 1
+
+#         return -1
+
+#     return -1
